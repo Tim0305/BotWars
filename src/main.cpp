9a28c82b8bded49c4e2b09313c307f200081211e
KevRamirezM <kevinkapi20@gmail.com>
2026-09-08 17:21:23 -0600

    Initial main commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
 int main() {
-    std::cout << "Hello world" << std::endl;
+    ifstream file("bitacora.txt");
+
+    if (!file.is_open()) {
+        cerr << "Error: Could not open the file" << endl;
+        return 1;
+    }
+    
+    string startDate;
+    string endDate;
+
+    cout << "Enter the start date in DD-MM format" << endl;
+    cin >> startDate;
+    
+    cout << "Enter the end date in DD-MM format" << endl;
+    cin >> endDate;
+
+    
+
     return 0;
 }
