3adec9647989041b837483404cf0307e0782a4e5
Omar Del Toro <omardelt006@gmail.com>
2026-09-08 16:44:14 -0600

    un commit

--- src/date.cpp
+++ src/date.cpp
@@ -38,14 +38,9 @@ bool operator>(Date d1, Date d2){
 }
 
 bool Date::operator<=(Date date){
-    if (this->year > date.getYear()) return false;
-        else if (this->month > date.getMonth()) return false;
-            else if (this->day > date.getDay()) return false;
-                else if (this->hour > date.getHour()) return false;
-                    else if (this->minutes > date.getMinutes()) return false;
-                        else if (this->seconds > date.getSeconds()) return false;
-                            else {
-                                return true;
-                            }
+    if (this->year <= date.getYear() && 
+        this->year <= date.getYear() &&
+        this->year <= date.getYear() &&                               )
+                            
 }
 
