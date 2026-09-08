b6352963035c77f7e7b2776f96a68c58ff308391
Timoteo <tabtim1015@gmail.com>
2026-09-08 16:40:21 -0600

    Log

--- src/date.cpp
+++ src/date.cpp
@@ -9,34 +9,18 @@ Date::Date(int day, Month month, int year, int hour, int minutes, int seconds) {
     this->seconds = seconds;
 }
 
-int Date::getDay() {
-    return this->day;
-}
+int Date::getDay() { return this->day; }
 
-Month Date::getMonth() {
-    return this->month;
-}
+Month Date::getMonth() { return this->month; }
 
-int Date::getYear() {
-    return this->year;
-}
+int Date::getYear() { return this->year; }
 
-int Date::getHour() {
-    return this->hour;
-}
+int Date::getHour() { return this->hour; }
 
-int Date::getMinutes() {
-    return this->minutes;
-}
+int Date::getMinutes() { return this->minutes; }
 
-int Date::getSeconds() {
-    return this->seconds;
-}
+int Date::getSeconds() { return this->seconds; }
 
-bool operator>(Date d1, Date d2){
-    
-}
+bool operator>(Date d1, Date d2) {}
 
-bool operator<=(Date Date){
-    
-}
+bool operator<=(Date date) {}
